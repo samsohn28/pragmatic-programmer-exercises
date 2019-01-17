6c4dbb1602299d865362bc0f5f233461c9f82a33
Samuel Sohn <sam.sohn28@gmail.com>
2019-01-16 21:21:18 -0800

    Exercise 1: Saying Hello

---
+++ Challenge1.py
@@ -0,0 +1,19 @@
+# Saying Hello - Create a program that prompts for your name and prints a greeting using your name
+# Constraints: Keep the input, string concatenation, and output separate
+# Date: 1/16/19
+
+# Challenges:   Write a new version of the program without using any variables
+#               Write a version of the program that displays different greetings for different people
+# Date:
+
+# Test case 1
+# Input: Sam
+# Output: Hello, Sam, nice to meet you!
+
+# Test case 2
+# Input: 12345
+# Output: Hello, 12345, nice to meet you!
+
+name = input('What is your name? ')
+greeting = 'Hello, ' + name + ', nice to meet you!'
+print(greeting)
